405630211764bdbaad9b5e09b65dfbe7001db0d3
Alex Zubrytskyi <zubrytskyi.aa@gmail.com>
2017-08-10 02:26:11 +0300

    ss

--- FinalHomeWork/src/model/main.java
+++ FinalHomeWork/src/model/main.java
@@ -13,7 +13,7 @@ public class main {
 	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
 		File urlCustomer = new File("d:\\Customers.csv");
 		File urlItem = new File("d:\\items.csv");
